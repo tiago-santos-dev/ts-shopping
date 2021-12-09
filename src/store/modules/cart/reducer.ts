c53aa353c6ae5d70781ada06cdfbbac74eabf3c5
Tiago Santos <trodrigues322@gmail.com>
2021-12-09 20:59:55 -0300

    Alterando Estado

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -4,10 +4,16 @@ import { ICartState } from "./types";
 const INITIAL_STATE: ICartState = {
     items: []
 }
-const cart: Reducer<ICartState> = (state, action) => {
-    console.log(state)
-    console.log(action)
-    return INITIAL_STATE;
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+
+    switch (action.type) {
+        case 'ADD_PRODUCT_TO_CART':
+            return state;
+
+
+        default:
+            return state;
+    }
 }
 
 export default cart;
